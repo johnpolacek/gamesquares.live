069d3079010a14ff55a211a5cac32e0f5066964a
John Polacek <johnpolacek@gmail.com>
2026-02-09 10:15:50 -0600

    disable cron job

--- convex/crons.ts
+++ convex/crons.ts
@@ -4,10 +4,11 @@ import { api } from "./_generated/api";
 const crons = cronJobs();
 
 // Fetch NFL scores from ESPN every minute on game day
-crons.interval(
-	"fetch nfl scores",
-	{ minutes: 1 },
-	api.nflScores.fetchAndUpdateScores,
-);
+// DISABLED: Super Bowl is over. Uncomment next year to re-enable automatic score updates.
+// crons.interval(
+// 	"fetch nfl scores",
+// 	{ minutes: 1 },
+// 	api.nflScores.fetchAndUpdateScores,
+// );
 
 export default crons;
